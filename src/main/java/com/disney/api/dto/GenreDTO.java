8faebe55d3619b2aee9ebbbe39ac9a7674c40188
Cristian <cristianfcba@gmail.com>
2022-02-09 17:37:01 -0300

    MovieFiltersDTO constructor and GenreDTO

---
+++ src/main/java/com/disney/api/dto/GenreDTO.java
@@ -0,0 +1,15 @@
+package com.disney.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GenreDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String image;
+}
--- src/main/java/com/disney/api/dto/MovieFiltersDTO.java
+++ src/main/java/com/disney/api/dto/MovieFiltersDTO.java
@@ -1,15 +1,17 @@
 package com.disney.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MovieFiltersDTO {
 
     private String title;
 
     private Long genre_id;
 
-    // TODO: 09/02/2022 order ASC/DESC 
+    // TODO: 09/02/2022 order ASC/DESC
 }
