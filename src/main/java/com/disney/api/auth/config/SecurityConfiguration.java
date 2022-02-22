f8d51f2151942a34e234fceaa48b50ef44e98643
Cristian <cristianfcba@gmail.com>
2022-02-21 23:22:50 -0300

    add jwt

--- src/main/java/com/disney/api/auth/config/SecurityConfiguration.java
+++ src/main/java/com/disney/api/auth/config/SecurityConfiguration.java
@@ -1,37 +1,49 @@
 package com.disney.api.auth.config;
 
-//import com.disney.api.auth.service.UserDetailsCustomService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import com.disney.api.auth.service.UserDetailsCustomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsCustomService userDetailsCustomService;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsCustomService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.csrf().disable()
+//                .authorizeRequests().antMatchers("/auth/*").permitAll()
+//                .anyRequest().authenticated()
+//                .and().exceptionHandling()
+//                .and().sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //
-//    @Autowired
-//    private UserDetailsCustomService userDetailsCustomService;
-//
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsCustomService);
+//        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 //    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @Override
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//
-//}
+
+
+
+}
--- src/main/java/com/disney/api/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/disney/api/auth/service/UserDetailsCustomService.java
@@ -1,32 +1,49 @@
 package com.disney.api.auth.service;
-//
-//import com.disney.api.auth.entity.UserEntity;
-//import com.disney.api.auth.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Collections;
+
+import com.disney.api.auth.dto.UserDTO;
+import com.disney.api.auth.entity.UserEntity;
+import com.disney.api.auth.repository.UserRepository;
+import com.disney.api.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 
-//@Service
-//public class UserDetailsCustomService implements UserDetailsService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-//
-//        UserEntity userEntity = userRepository.findByUsername(userName);
-//        if (userEntity == null) {
-//            throw new UsernameNotFoundException("Username or password not found");
-//        }
-//        return new User(userEntity.getUsername(), userEntity.getPassword(), Collections.emptyList());
-//    }
-//
-//
-//}
+@Service
+public class UserDetailsCustomService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByUsername(userName);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("Username or password not found.");
+        }
+        return new User(userEntity.getUsername(), userEntity.getPassword(), Collections.emptyList());
+    }
+
+    public boolean createUser(UserDTO userDTO)  {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(userDTO.getUsername());
+        userEntity.setPassword(userDTO.getPassword());
+        userEntity = userRepository.save(userEntity);
+
+        if (userEntity != null) {
+            emailService.SendWelcomeEmailTo(userEntity.getUsername());
+        }
+        return userEntity != null;
+    }
+
+}
